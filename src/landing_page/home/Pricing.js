404d5093bd0eb6ee90ff3879542ea87c08bdfe3b
Rakshita Naik <rakshitanaik306@gmail.com>
2026-03-15 21:19:51 +0530

    add-home

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,7 +2,29 @@ import React from 'react';
 function Pricing
 () {
     return ( 
-        <h1>Pricing</h1>
+        <div className='container '>
+            <div className='row'>
+                <div className='col-4'>
+                    <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a  className='mx-5'style={{textDecoration:"none"}} href=''>See pricing <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+                <div className='col-2'></div>
+                <div className='col-6 mb-5'>
+                    <div className='row text-center'>
+                    <div className='col p-3 border'>
+                        <h1 className='mb-3'>₹0</h1>
+                        <p>Free account opening</p>
+                    </div>
+                    <div className='col p-3 border'>
+                        <h1>₹20</h1>
+                        <p>Intraday and F&O</p>
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+        </div>
      );
 }
 
