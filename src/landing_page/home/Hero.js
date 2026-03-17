d1baebe8a8bba3a7b34d049dcb81157900e8f842
Rakshita Naik <rakshitanaik306@gmail.com>
2026-03-17 22:59:36 +0530

    added-openaccount-footer

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -5,8 +5,8 @@ function Hero() {
             <div className='row text-center'>
                 <img src='media/homeHero.png' alt='heroimg' className='mb-5'/>
                 <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online Platform to invest in stocks,derivatives,mutual funds,and more</p>
-                <button className='p-2 btn btn-primary fs-5 mb-5'style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
+                <p className='mt-3'>Online Platform to invest in stocks,derivatives,mutual funds,and more</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5 mt-3'style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
             </div>
         </div>
             
