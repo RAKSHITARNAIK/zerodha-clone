79226c87e229cc9a98d9758cf5b8190085426425
Rakshita Naik <rakshitanaik306@gmail.com>
2026-09-21 20:57:56 +0530

    Updated holdings component and dependencies

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { all } from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
 // import { holdings } from "../data/data";
@@ -9,7 +9,7 @@ const Holdings = () => {
 
   useEffect(() => {
     axios.get("http://localhost:3002/allHoldings").then((res) => {
-      // console.log(res.data);
+      console.log(res.data);
       setAllHoldings(res.data);
     });
   }, []);
